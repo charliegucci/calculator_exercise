18e202d4a62f7b811d80ff07ee19c85f45bd7643
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2019-11-09 14:38:48 +1000

    quit the loop

--- exercise.rb
+++ exercise.rb
@@ -1,7 +1,17 @@
-loop do
+quit = false
+until quit do
     puts "Options"
     puts "1. Sum"
     puts "2. Substract"
     puts "3. Divide"
     puts "4. Multiply"
+    user_input = gets.chomp
+    case user_input
+    when "q"
+        quit = true
+        
 end
+
+Requirements
+    to be able to quit
+    takes to value
